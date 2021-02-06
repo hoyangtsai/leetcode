15617f6e9b985ca46cd8dadca60f5f7590c6563c
Hoyang Tsai <hoyang.t@gmail.com>
2021-02-06 21:36:35 +0800

    Add q.904 solution

--- 904.fruit-into-baskets.js
+++ 904.fruit-into-baskets.js
@@ -4,13 +4,34 @@
  * [904] Fruit Into Baskets
  */
 
+// #slideWindow, #twoPointers
+
 // @lc code=start
 /**
  * @param {number[]} tree
  * @return {number}
  */
 var totalFruit = function(tree) {
-    
+  const bucket = new Map();
+
+  let left = 0;
+  let maxLen = 0;
+  
+  for (let right = 0; right < tree.length; right++) {
+    // appear times
+    bucket.set(tree[right], isNaN(bucket.get(tree[right])) ? 1 : bucket.get(tree[right]) + 1);
+
+    while (bucket.size >= 3) {
+      bucket.set(tree[left], isNaN(bucket.get(tree[left])) ? 1 : bucket.get(tree[left]) - 1);
+      if (bucket.get(tree[left]) == 0) {
+        bucket.delete(tree[left]);
+      }
+      left++;
+    }
+
+    maxLen = Math.max(maxLen, right - left + 1);
+  }
+
+  return maxLen;
 };
 // @lc code=end
-
