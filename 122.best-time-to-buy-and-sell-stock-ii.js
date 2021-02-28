62631767c70f84b2a363e75b15599b11d4cba28f
Hoyang Tsai <hoyang.t@gmail.com>
2021-02-28 09:58:04 +0800

    new quiz and solution

---
+++ 122.best-time-to-buy-and-sell-stock-ii.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=122 lang=javascript
+ *
+ * [122] Best Time to Buy and Sell Stock II
+ */
+
+ // #array, #greedy
+
+ // tips: cannot sell and buy at the same day
+ 
+// @lc code=start
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  let maxprofit = 0;
+  for (let i = 1; i < prices.length; i++) {
+    // if next day greater than the previous day
+    if (prices[i] > prices[i - 1]) {
+      maxprofit += prices[i] - prices[i - 1];
+    }
+  }
+  return maxprofit;
+};
+// @lc code=end
